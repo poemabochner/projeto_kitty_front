f4daf5a15284a555cfed5dd3d1267514dbccd31b
Poema Bochner <poemabochner8@gmail.com>
2024-01-17 18:44:55 -0300

    cria método pra expandir icon no hover

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="header is-flex p-4">
+  <div class="header is-flex py-5 pr-6 pl-4 is-justify-content-space-between">
     <img class="header__logo" src="@/assets/images/logo.png" />
-    <LoggedUserComponent />
-    <img class="header__icon" src="@/assets/icons/logout.svg" />
+    <div class="is-flex is-align-items-center" style="gap: 5rem;">
+      <LoggedUserComponent />
+      <img class="header__icon" src="@/assets/icons/logout.svg"  @mouseover="expandIcon" @mouseout="shrinkIcon" />
+    </div>
   </div>
 </template>
 
@@ -13,6 +15,22 @@ export default {
   name: 'HeaderComponent',
   components: {
     LoggedUserComponent,
+  },
+
+  data() {
+    return {
+      isIconExpanded: false,
+    }
+  },
+
+  methods: {
+    expandIcon() {
+      this.isIconExpanded = true
+    },
+
+    shrinkIcon() {
+      this.isIconExpanded = false
+    }
   }
 }
 </script>
@@ -24,11 +42,18 @@ export default {
 }
 
 .header__logo {
-  width: 7%;
+  width: 150px;
 }
 
 .header__icon {
-  width: 2%;
+  width: 50px;
   filter: var(--filtro-branco);
+  transition: transform 0.3s ease;
+}
+
+.header__icon:hover {
+  cursor: pointer;
+  filter: invert(64%) sepia(32%) saturate(1390%) hue-rotate(302deg) brightness(100%) contrast(104%);
+  transform: scale(1.2);
 }
 </style>
