19830748a64bac5ced5b7e41e38dedd61749e22a
Poema Bochner <poemabochner8@gmail.com>
2024-01-17 23:10:46 -0300

    add rota de cliente

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from './views/LoginView.vue'
+import ClientView from './views/ClientView.vue'
 
 const routes = [
   { path: '/', component: LoginView, meta: { showHeader: false, showFooter: false }},
+  { path: '/cliente', component: ClientView, meta: { showHeader: true, showFooter: true }}
 ]
 
 const router = createRouter({
