54e5c789d3aaa79a6348a4e0b14107e276214055
Poema Bochner <poemabochner8@gmail.com>
2024-01-18 18:14:42 -0300

    usa ModalComponent

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,23 +1,39 @@
 <template>
-  <div class="is-flex is-justify-content-center" style="gap: 3em;">
-    <CardComponent texto="nossos lanches!"><img src="@/assets/images/cliente.jpg"/></CardComponent>
-    <CardComponent texto="nossas promoções! "><img src="@/assets/images/cliente.jpg"/></CardComponent>
-    <CardComponent texto="monte seu lanche!"><img src="@/assets/images/cliente.jpg"/></CardComponent>
+  <div>
+    <ModalComponent :isModalActive="isModalActive" @closeModal="closeModal"></ModalComponent>
+    <div class="is-flex is-justify-content-center" style="gap: 3rem;">
+      <CardComponent texto="nossos lanches!" @click="openModal('lanche')"><img src="@/assets/images/cliente.jpg" /></CardComponent>
+      <CardComponent texto="nossas promoções!" @click="openModal('promocao')"><img src="@/assets/images/cliente.jpg" /></CardComponent>
+      <CardComponent texto="monte seu lanche!" @click="openModal('monteSeuLanche')"><img src="@/assets/images/cliente.jpg" /></CardComponent>
+    </div>
   </div>
 </template>
 
 <script>
 import CardComponent from '@/components/CardComponent.vue';
+import ModalComponent from '@/components/ModalComponent.vue';
 
 export default {
   name: 'ClientView',
   components: {
-    CardComponent
+    CardComponent,
+    ModalComponent
   },
-
   data() {
     return {
-      imagemSrc: '@/assets/images/cliente.jpg'
+      imagemSrc: '@/assets/images/cliente.jpg',
+      isModalActive: false,
+      modalType: null
+    }
+  },
+  methods: {
+    openModal(type) {
+      this.isModalActive = true,
+      this.modalType = type
+    },
+    closeModal() {
+      this.isModalActive = false,
+      this.modalType = null
     }
   }
 }
