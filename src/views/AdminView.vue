2bdef8d34f26016d4cd8806939c86c04abb47bf7
Poema Bochner <poemabochner8@gmail.com>
2024-01-18 19:12:57 -0300

    usa DataTableComponent na AdminView

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
-
+  <div class="is-flex is-justify-content-center mt-6">
+    <DataTableComponent tableTitle="ingredientes" :tableData="tableData" />
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'AdminView'
+import DataTableComponent from '@/components/DataTableComponent.vue';
+
+export default {
+  components: {
+    DataTableComponent
+  },
+  data() {
+    return {
+      tableData: [
+        { id: 'linguiça', name: 'R$: 2,99'},
+        { id: 'linguiça', name: 'R$: 2,99'},
+        { id: 'linguiça', name: 'R$: 2,99'},
+        { id: 'linguiça', name: 'R$: 2,99'},
+        { id: 'linguiça', name: 'R$: 2,99'},
+        { id: 'linguiça', name: 'R$: 2,99'},
+      ]
+    };
   }
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
 </style>
