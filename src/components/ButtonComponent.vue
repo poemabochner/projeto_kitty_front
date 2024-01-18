44f4a669e8cb4f2f4e4380ae16e7defe1fd3d554
Poema Bochner <poemabochner8@gmail.com>
2024-01-18 20:31:28 -0300

    cria ButtonComponent

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="button__container p-2 has-text-weight-semibold has-text-centered m-3">
+    adicionar
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ButtonComponent',
+  }
+</script>
+
+<style scoped>
+  .button__container {
+    background-color: var(--cor-hover);
+    width: 8rem;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+
+  .button__container:hover{
+    background-color: var(--rosa-escuro);
+  }
+</style>
