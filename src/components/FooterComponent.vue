7f3f867c75ac78d5bb1d57eb4b5d2e3e15c68140
Poema Bochner <poemabochner8@gmail.com>
2024-01-18 20:32:01 -0300

    ajusta FooterComponent

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,8 +1,7 @@
 <template>
   <footer v-if="showFooter" class="footer py-4 is-flex is-justify-content-space-between has-text-weight-semibold">
     <div class="footer__lado-esquerdo is-flex is-align-items-center" style="gap: 2rem;">
-      <img class="footer__lado-esquerdo-icon" src="@/assets/icons/instagram.svg" alt="ícone do instagram"
-        ref="iconRef1" />
+      <img class="footer__lado-esquerdo-icon" src="@/assets/icons/instagram.svg" alt="ícone do instagram" ref="iconRef1" />
       <img class="footer__lado-esquerdo-icon" src="@/assets/icons/tiktok.svg" alt="ícone do tiktok" ref="iconRef2" />
       <img class="footer__lado-esquerdo-icon" src="@/assets/icons/whatsapp.svg" alt="ícone do whatsapp" ref="iconRef3" />
     </div>
@@ -22,22 +21,22 @@ export default {
     })
   },
 
-    methods: {
-      addIconListeners() {
-        const iconRef1 = this.$refs.iconRef1
-        const iconRef2 = this.$refs.iconRef2
-        const iconRef3 = this.$refs.iconRef3
+  methods: {
+    addIconListeners() {
+      const iconRef1 = this.$refs.iconRef1
+      const iconRef2 = this.$refs.iconRef2
+      const iconRef3 = this.$refs.iconRef3
 
-        if (iconRef1) {
-          iconExpand(iconRef1)
-        }
-        if (iconRef2) {
-          iconExpand(iconRef2)
-        }
-        if (iconRef3) {
-          iconExpand(iconRef3)
-        }
-      },
+      if (iconRef1) {
+        iconExpand(iconRef1)
+      }
+      if (iconRef2) {
+        iconExpand(iconRef2)
+      }
+      if (iconRef3) {
+        iconExpand(iconRef3)
+      }
+    },
   },
 
   computed: {
@@ -56,8 +55,8 @@ export default {
   background-color: var(--cor-hover);
   padding-right: 5rem;
   padding-left: 5rem;
-  position: fixed;
   width: 100%;
+  position: absolute;
   bottom: 0;
 }
 
