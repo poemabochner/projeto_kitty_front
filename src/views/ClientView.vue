8855fd02b9e5363c1d000d1cc490b65394a801b7
Poema Bochner <poemabochner8@gmail.com>
2024-01-18 07:55:12 -0300

    usa CardComponent na ClientView

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    oi
+  <div class="is-flex is-justify-content-center" style="gap: 3em;">
+    <CardComponent texto="nossos lanches!"><img src="@/assets/images/cliente.jpg"/></CardComponent>
+    <CardComponent texto="nossas promoções! "><img src="@/assets/images/cliente.jpg"/></CardComponent>
+    <CardComponent texto="monte seu lanche!"><img src="@/assets/images/cliente.jpg"/></CardComponent>
   </div>
 </template>
 
 <script>
-  export default {
-    
+import CardComponent from '@/components/CardComponent.vue';
+
+export default {
+  name: 'ClientView',
+  components: {
+    CardComponent
+  },
+
+  data() {
+    return {
+      imagemSrc: '@/assets/images/cliente.jpg'
+    }
   }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
