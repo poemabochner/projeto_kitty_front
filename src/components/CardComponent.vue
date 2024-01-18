f8c1957270eb67fb1030006e1c127d7248e383fc
Poema Bochner <poemabochner8@gmail.com>
2024-01-18 07:54:57 -0300

    cria CardComponent

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card has-text-weight-semibold has-text-centered my-5">
+    <div class="card__texto p-4">
+      <p>{{ texto }}</p>
+    </div>
+
+    <div class="card-image">
+      <figure class="image">
+        <slot></slot>
+      </figure>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent',
+  props: {
+    texto: {
+      type: String,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  background-color: var(--cor-hover);
+  width: 12rem;
+}
+
+.card:hover{
+  cursor: pointer;
+}
+</style>
