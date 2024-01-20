66ed9ac6d4c0e6c9b73bc820a557f9e3620b04ff
Poema Bochner <poemabochner8@gmail.com>
2024-01-20 00:53:39 -0300

    cria ToastComponent

---
+++ src/components/ToastComponent.vue
@@ -0,0 +1,64 @@
+<template>
+  <transition name="fade">
+    <div v-if="show" class="toast is-flex p-5 tag is-primary is-light is-large" style="gap: 4px;">
+      <img src="@/assets/icons/oktoast.svg" style="width: 24px;"/>
+      {{ message }}
+
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    }
+  },
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    showAutomaticToast() {
+      const toastShown = localStorage.getItem('toastShown');
+
+      if (!toastShown) {
+        this.show = true;
+
+        localStorage.setItem('toastShown', 'true');
+
+        setTimeout(() => {
+          this.show = false;
+        }, 3000);
+      }
+    },
+  },
+  mounted() {
+    this.showAutomaticToast();
+  },
+}
+</script>
+
+<style scoped>
+.toast {
+  position: fixed;
+  top: 100px;
+  /* Ajuste a posição conforme necessário */
+  right: 100px;
+  /* Ajuste a posição conforme necessário */
+  z-index: 9999;
+  /* Garante que o Toast fique no topo */
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}</style>
