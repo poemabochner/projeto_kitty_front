0e06d3225735d60bfa8d09303ef4d7b7ace355de
Poema Bochner <poemabochner8@gmail.com>
2024-01-19 22:32:01 -0300

    add rotas para novas views

--- src/router.js
+++ src/router.js
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from './views/LoginView.vue'
 import ClientView from './views/ClientView.vue'
 import AdminView from './views/AdminView.vue'
+import LanchesView from './views/LanchesView.vue'
+import PromocoesView from './views/PromocoesView.vue'
 
 const routes = [
-  { path: '/', component: LoginView, meta: { showHeader: false, showFooter: false }},
+  { path: '/login', component: LoginView, meta: { showHeader: false, showFooter: false }},
   { path: '/cliente', component: ClientView, meta: { showHeader: true, showFooter: true }},
-  { path: '/administrador', component: AdminView, meta: { showHeader: true, showFooter: true }}
+  { path: '/administrador', component: AdminView, meta: { showHeader: true, showFooter: true }},
+  { path: '/lanches', component: LanchesView, meta: { showHeader: true, showFooter: true }},
+  { path: '/promocoes', component: PromocoesView, meta: { showHeader: true, showFooter: true }}
 ]
 
 const router = createRouter({
