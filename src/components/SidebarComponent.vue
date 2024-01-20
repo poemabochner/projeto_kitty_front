7702fc4cbb88dfcc9b7bc0d25de25ade2d7075f5
Poema Bochner <poemabochner8@gmail.com>
2024-01-20 00:53:25 -0300

    ajusta SidebarComponent

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="is-flex m-3" style="gap: 0.7rem;">
-    <img class="sidebar-img p-2" src="@/assets/icons/ingredientes.svg" alt="" @click="$router.push('/administrador')">
-    <img class="sidebar-img p-2" src="@/assets/icons/lanches.svg" alt="" @click="$router.push('/lanches')">
-    <img class="sidebar-img p-2" src="@/assets/icons/promocao.svg" alt="" @click="$router.push('/promocoes')">
+  <div class="is-flex m-3 is-flex-direction-column" style="gap: 0.7rem;">
+    <div class="sidebar-img-container" @click="$router.push('/administrador')" title="Administrador">
+      <img class="sidebar-img p-2" src="@/assets/icons/ingredientes.svg" alt="">
+      <span class="tooltip">Administrador</span>
+    </div>
+    <div class="sidebar-img-container" @click="$router.push('/lanches')" title="Lanches">
+      <img class="sidebar-img p-2" src="@/assets/icons/lanches.svg" alt="">
+      <span class="tooltip">Lanches</span>
+    </div>
+    <div class="sidebar-img-container" @click="$router.push('/promocoes')" title="Promoções">
+      <img class="sidebar-img p-2" src="@/assets/icons/promocao.svg" alt="">
+      <span class="tooltip">Promoções</span>
+    </div>
   </div>
 </template>
 
@@ -13,6 +22,10 @@ export default {
 </script>
 
 <style scoped>
+.sidebar-img-container {
+  position: relative;
+}
+
 .sidebar-img {
   background-color: var(--cor-hover);
   border-radius: 10px;
@@ -22,5 +35,21 @@ export default {
 
 .sidebar-img:hover {
   background-color: var(--rosa-escuro);
+}
+
+.tooltip {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  transform: translateX(50%);
+  background-color: var(--rosa-claro);
+  color: var(--preto-principal);
+  border-radius: 5px;
+  padding: 10px;
+  display: none;
+}
+
+.sidebar-img-container:hover .tooltip {
+  display: block;
 }
 </style>
