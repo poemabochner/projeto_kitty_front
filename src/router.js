8bc8be757d0b9bec201da9bc61e41be7ff509f24
Poema Bochner <poemabochner8@gmail.com>
2024-01-21 21:04:51 -0300

    ajusta rotas

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from './views/LoginView.vue'
 import ClientView from './views/ClientView.vue'
-import AdminView from './views/AdminView.vue'
+import IngredientesView from './views/IngredientesView.vue'
 import LanchesView from './views/LanchesView.vue'
 import PromocoesView from './views/PromocoesView.vue'
 
 const routes = [
   { path: '/login', component: LoginView, meta: { showHeader: false, showFooter: false }},
   { path: '/cliente', component: ClientView, meta: { showHeader: true, showFooter: true }},
-  { path: '/administrador', component: AdminView, meta: { showHeader: true, showFooter: true }},
+  { path: '/ingredientes', component: IngredientesView, meta: { showHeader: true, showFooter: true }},
   { path: '/lanches', component: LanchesView, meta: { showHeader: true, showFooter: true }},
   { path: '/promocoes', component: PromocoesView, meta: { showHeader: true, showFooter: true }}
 ]
