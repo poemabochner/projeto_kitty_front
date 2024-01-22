88955b463f0199c41c000c733e84cc909341a57d
Poema Bochner <poemabochner8@gmail.com>
2024-01-22 17:40:44 -0300

    atribui nome dependendo da rota

--- src/components/LoggedUserComponent.vue
+++ src/components/LoggedUserComponent.vue
@@ -1,28 +1,40 @@
 <template>
   <div class="usuario-logado is-flex py-3 pr-4 pl-3 is-align-items-center has-text-weight-semibold">
     <img class="usuario-logado__icon" src="@/assets/icons/user.svg"/>
-    <p>cliente</p>
+    <p>{{ user }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'LoggedUserComponent',
+  computed: {
+    user() {
+      const currentPath = this.$route.path;
 
+      if (currentPath.includes('/cliente')) {
+        return 'cliente'
+      } else if (currentPath.includes('/promocoes')|| currentPath.includes('/lanches')|| currentPath.includes('/ingredientes')) {
+        return 'administrador'
+      } else {
+        return 'desconhecido'
+      }
+    },
+  },
 }
 </script>
 
 <style scoped>
-  .usuario-logado{
-    color: var(--branco);
-    background-color: var(--rosa-detalhes);
-    border: 2px solid var(--branco);
-    border-radius: 10px;
-    height: 3rem;
-    gap: 0.5rem;
-  }
+.usuario-logado {
+  color: var(--branco);
+  background-color: var(--rosa-detalhes);
+  border: 2px solid var(--branco);
+  border-radius: 10px;
+  height: 3rem;
+  gap: 0.5rem;
+}
 
-  .usuario-logado__icon {
-    width: 25px;
-  }
+.usuario-logado__icon {
+  width: 25px;
+}
 </style>
