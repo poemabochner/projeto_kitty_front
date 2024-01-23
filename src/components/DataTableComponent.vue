18bac04ec69c11b93fadc270e085e0b2a857920e
Poema Bochner <poemabochner8@gmail.com>
2024-01-23 00:03:50 -0300

    termina de integrar com o back

--- src/components/DataTableComponent.vue
+++ src/components/DataTableComponent.vue
@@ -27,7 +27,8 @@
           </div>
 
           <div class="is-flex" style="gap: 1rem;">
-            <ButtonComponent textoBotao="salvar" @click="salvar"><img src="@/assets/icons/check.svg" /></ButtonComponent>
+            <ButtonComponent textoBotao="salvar" @click="salvarIngrediente"><img src="@/assets/icons/check.svg" />
+            </ButtonComponent>
             <ButtonComponent textoBotao="cancelar" @click="cancelar"><img src="@/assets/icons/close.svg" />
             </ButtonComponent>
           </div>
@@ -51,7 +52,8 @@
           </div>
 
           <div class="is-flex" style="gap: 1rem;">
-            <ButtonComponent textoBotao="salvar" @click="salvar"><img src="@/assets/icons/check.svg" /></ButtonComponent>
+            <ButtonComponent textoBotao="salvar" @click="salvarIngrediente"><img src="@/assets/icons/check.svg" />
+            </ButtonComponent>
             <ButtonComponent textoBotao="cancelar" @click="cancelar"><img src="@/assets/icons/close.svg" />
             </ButtonComponent>
           </div>
@@ -75,8 +77,9 @@
         <td class="table-cell-maior">{{ ingrediente.nomeIngrediente }}</td>
         <td class="table-cell">{{ formatarPreco(ingrediente.precoIngrediente) }}</td>
         <td class="is-flex table-cell is-justify-content-flex-end" style="gap: 0.6rem;">
-          <img class="icons" src="@/assets/icons/edit.svg" @click="openModal('editar')" style="width: 26px;" />
-          <img class="icons" src="@/assets/icons/delete.svg" @click="openModal('excluir')" />
+          <img class="icons" src="@/assets/icons/edit.svg" @click="openModal('editar', ingrediente)"
+            style="width: 26px;" />
+          <img class="icons" src="@/assets/icons/delete.svg" @click="openModal('excluir', ingrediente)" />
         </td>
       </tr>
       <div class="is-flex is-justify-content-flex-end pr-2"
@@ -111,20 +114,27 @@ export default {
       modalTitle: '',
       nomeIngrediente: '',
       precoIngrediente: '',
-      porcentagemDesconto: '',
-      ingredientes: []
-    };
+      ingredientes: [],
+      idIngredienteExcluir: null,
+      idIngredienteEditar: null
+    }
   },
   methods: {
-    openModal(type) {
+    openModal(type, ingrediente) {
       this.isModalActive = true
       this.modalType = type
 
       if (type === 'excluir') {
-        this.modalTitle = `Deseja mesmo excluir o ingrediente ${this.nome}?`
+        this.idIngredienteExcluir = ingrediente.idIngrediente
+        this.modalTitle = `Deseja mesmo excluir o ingrediente ${ingrediente.nomeIngrediente}?`
       } else if (type === 'adicionar') {
         this.modalTitle = 'Adicionar Ingrediente'
+        this.nomeIngrediente = ''
+        this.precoIngrediente = ''
       } else if (type === 'editar') {
+        this.idIngredienteEditar = ingrediente.idIngrediente
+        this.nomeIngrediente = ingrediente.nomeIngrediente
+        this.precoIngrediente = ingrediente.precoIngrediente
         this.modalTitle = 'Editar Ingrediente'
       }
     },
@@ -135,37 +145,104 @@ export default {
     cancelar() {
       this.closeModal()
     },
-    excluir() {
-      console.log('Ingrediente excluído:', this.nome)
+
+    atualizar(ingrediente) {
+      ingredienteService.atualizar(ingrediente)
+        .then(() => {
+          this.carregarIngredientes()
+          alert(`Ingrediente ${this.nomeIngrediente} editado com sucesso!`)
+        })
+        .catch((error) => {
+
+          alert('Erro ao editar ingrediente', error)
+        })
+
       this.closeModal()
+    },
+
+    excluir() {
+      if (this.modalType === 'excluir') {
+        const idIngrediente = this.idIngredienteExcluir
+        if (!idIngrediente) {
+          alert('ID do ingrediente a ser excluído não encontrado.')
+          return
+        }
+        ingredienteService.deletar(idIngrediente)
+          .then(() => {
+            this.ingredientes = this.ingredientes.filter(ingrediente => ingrediente.id !== idIngrediente)
+            this.carregarIngredientes()
+            alert(`Ingrediente excluído com sucesso!`)
+          })
+          .catch(() => {
+            alert('O ingrediente não pode ser excluído, pois faz parte de um lanche!')
+          })
+          .finally(() => {
+            this.closeModal()
+            this.idIngredienteExcluir = null
+          })
+      }
     },
     salvar() {
       console.log('Ingrediente salvo:', {
         nome: this.nomeIngrediente,
         preco: this.precoIngrediente,
-        porcentagemDesconto: this.porcentagemDesconto,
       })
       this.closeModal()
     },
 
     validarNumero() {
       if (this.precoIngrediente < 0) {
-        this.precoIngrediente = 0;
+        this.precoIngrediente = 0
       }
     },
     async carregarIngredientes() {
       try {
         this.ingredientes = await ingredienteService.obterTodos()
       } catch (error) {
-        console.error('Erro ao obter ingredientes:', error)
+        alert('Erro ao obter ingredientes:', error)
       }
     },
-    formatarPreco(valor){
+    formatarPreco(valor) {
       return formatarPreco(valor)
+    },
+    salvarIngrediente() {
+      if (!this.nomeIngrediente || !this.precoIngrediente) {
+        alert('Por favor, preencha todos os campos.')
+        return
+      }
+
+      const novoIngrediente = {
+        idIngrediente: null,
+        nomeIngrediente: this.nomeIngrediente,
+        precoIngrediente: this.precoIngrediente,
+      }
+      if (this.modalType === 'adicionar') {
+        this.cadastrar(novoIngrediente)
+      } else {
+        novoIngrediente.idIngrediente = this.idIngredienteEditar
+        this.atualizar(novoIngrediente)
+      }
+
+    },
+
+    cadastrar(ingrediente) {
+      this.ingredientes.push(ingrediente)
+
+      ingredienteService.cadastrar(ingrediente)
+        .then(() => {
+          alert(`Ingrediente ${this.nomeIngrediente} cadastrado com sucesso!`)
+        })
+        .catch((error) => {
+          this.ingredientes.pop()
+
+          alert('Erro ao cadastrar ingrediente', error)
+        })
+
+      this.closeModal()
     },
   },
   mounted() {
-    this.carregarIngredientes();
+    this.carregarIngredientes()
   },
 }
 </script>
@@ -180,27 +257,27 @@ table {
 }
 
 .table-container {
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    width: 100%;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 100%;
+}
 
-  .table-row {
-    display: flex;
-    justify-content: space-between;
-  }
+.table-row {
+  display: flex;
+  justify-content: space-between;
+}
 
-  .table-cell-maior{
-    flex: 2;
-    min-width: 200px;
-    margin-right: 0.6rem;
-  }
+.table-cell-maior {
+  flex: 2;
+  min-width: 200px;
+  margin-right: 0.6rem;
+}
 
-  .table-cell {
-    flex: 1;
-    margin-right: 0.6rem;
-  }
+.table-cell {
+  flex: 1;
+  margin-right: 0.6rem;
+}
 
 th,
 td {
