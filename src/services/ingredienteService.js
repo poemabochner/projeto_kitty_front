e38891f061c84c80d3937a65e1745ee0b4082339
Poema Bochner <poemabochner8@gmail.com>
2024-01-23 00:00:07 -0300

    cria método de atualizar e cadastrar

--- src/services/ingredienteService.js
+++ src/services/ingredienteService.js
@@ -6,10 +6,22 @@ const obterTodos = () => {
     .catch((error) => Promise.reject(error))
 }
 
+const cadastrar = (ingrediente) => {
+  return api.post('/api/ingrediente', ingrediente)
+      .then((response) => response.data)
+      .catch((error) => Promise.reject(error))
+}
+
+const atualizar = (ingrediente) => {
+  return api.put(`api/ingrediente/${ingrediente.idIngrediente}`, ingrediente)
+      .then((response) => response.data)
+      .catch((error) => Promise.reject(error))
+}
+
 const deletar = (id) => {
   return api.delete(`/api/ingrediente/${id}`)
   .then((response) => response.data)
   .catch((error) => Promise.reject(error))
 }
 
-export default { obterTodos, deletar }
+export default { obterTodos, deletar, cadastrar, atualizar }
